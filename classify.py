06f14840414acd8abe45843a96cd29f1d8a7d4ce
Faris Zahrah <fariszahrah@gmail.com>
2020-02-25 15:56:15 -0600

    initial structure for class and clus

---
+++ classify.py
@@ -0,0 +1,4 @@
+# This serves as an inital classification model for building complaints, 
+# with the target feature being whether the violation was enforced or not.
+# Can we predict enforcment based on building complaint featueres is the high
+# level idea here.
